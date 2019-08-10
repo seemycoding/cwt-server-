ae210c7e0370adfd8c4c9bb798ac3aa80acc70e0
menavneet <menavneet@github.com>
2019-08-10 12:53:47 +0530

    test2

--- route/routes.js
+++ route/routes.js
@@ -55,7 +55,7 @@ router.get('/getQuestionCountOf', (req, res, next) => {
 
 
 router.get('/getQuestionCountOf/:type', (req, res, next) => {
-    res.end(questions.count({ type: req.params.type }));
+    res.end(JSON.stringify(questions.count({ type: req.params.type })));
 })
 
 //get knowldege
