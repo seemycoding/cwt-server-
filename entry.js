54f769d0464226abd661a9af6036b0d5d5bfc7e0
Himanshu <hvk940@gmail.com>
2019-08-10 14:21:51 +0530

    push

--- entry.js
+++ entry.js
@@ -23,7 +23,7 @@ const PORT = 3000;
 //middleware
 //const directory = path.join(__dirname, "/uploads");
 //app.use("/uploads", express.static(directory));
-app.use("/upload", express.static("upload"));
+app.use("/uploads", express.static("uploads"));
 app.use(cors());
 app.use(bodyparser.json());
 
--- route/routes.js
+++ route/routes.js
@@ -11,7 +11,7 @@ const highlights = require("../model/highlights");
 //storage defination
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, "./upload");
+    cb(null, "./uploads");
   },
   filename: function(req, file, cb) {
     cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
