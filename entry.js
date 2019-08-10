5e54f1cef5ba50b413fd8e96c4451cdbed100dfc
menavneet <menavneet@github.com>
2019-08-10 12:22:05 +0530

    Add Question Count

--- entry.js
+++ entry.js
@@ -11,27 +11,25 @@ const route = require("./route/routes");
 mongoose.connect("mongodb://localhost:27017/cwt");
 //on connection
 mongoose.connection.on("connected", () => {
-  console.log("mongodb connected at port 27017");
+    console.log("mongodb connected at port 27017");
 });
 //on connection error
 mongoose.connection.on("error", err => {
-  console.log(err);
+    console.log(err);
 });
 
 const PORT = 3000;
 
 //middleware
 app.use("/uploads", express.static("uploads"));
-//app.use("/uploads", route);
 app.use(cors());
-//body-parser
 app.use(bodyparser.json());
 
 app.use("/api", route);
 app.get("/", (req, res) => {
-  res.send("foobar");
+    res.send("foobar");
 });
 
 app.listen(PORT, () => {
-  console.log("server has been started on port: " + PORT);
+    console.log("server has been started on port: " + PORT);
 });
--- route/routes.js
+++ route/routes.js
@@ -9,173 +9,178 @@ const news = require("../model/news");
 const questions = require("../model/knowledge");
 const highlights = require("../model/highlights");
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, "./uploads");
-  },
-  filename: function(req, file, cb) {
-    cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
-  }
+    destination: function(req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function(req, file, cb) {
+        cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
+    }
 });
 
 const filefilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
 };
 const upload = multer({
-  storage: storage,
-  fileFilter: filefilter
+    storage: storage,
+    fileFilter: filefilter
 });
 //get articles
 router.get("/articles", (req, res, next) => {
-  article.find(function(err, articles) {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(articles);
-    }
-  });
+    article.find(function(err, articles) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(articles);
+        }
+    });
 });
 //get news
 router.get("/news", (req, res, next) => {
-  news.find(function(err, news) {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(news);
-    }
-  });
+    news.find(function(err, news) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(news);
+        }
+    });
 });
+
+router.get('/getQuestionCountOf/:type', (req, res, next) => {
+    res.end(questions.count({ type: req.params.type }));
+})
+
 //get knowldege
 router.get("/knowledge/:id", (req, res, next) => {
-  questions.find({ type: req.params.id }, (err, questions) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(questions);
-    }
-  });
+    questions.find({ type: req.params.id }, (err, questions) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(questions);
+        }
+    });
 });
 //get highlights
 router.get("/getHighlights", (req, res, next) => {
-  highlights.find(function(err, highlights) {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(highlights);
-    }
-  });
+    highlights.find(function(err, highlights) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(highlights);
+        }
+    });
 });
 
 //get image path
 
 //insert new highlights
 router.post("/highlights", upload.single("image"), (req, res, next) => {
-  let story = new highlights({
-    image: req.file.path.replace("\\", "/"),
-    title: req.body.title
-  });
-  story.save((err, highlights) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json({
-        message: "File uploaded successfully",
-        story
-      });
-    }
-  });
+    let story = new highlights({
+        image: req.file.path.replace("\\", "/"),
+        title: req.body.title
+    });
+    story.save((err, highlights) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json({
+                message: "File uploaded successfully",
+                story
+            });
+        }
+    });
 });
 
 //insert new article
 router.post("/ArticlePost", (req, res, next) => {
-  let newArticle = new article({
-    name: req.body.name,
-    profession: req.body.profession,
-    title: req.body.title,
-    detail: req.body.detail,
-    expert: req.body.expert
-  });
+    let newArticle = new article({
+        name: req.body.name,
+        profession: req.body.profession,
+        title: req.body.title,
+        detail: req.body.detail,
+        expert: req.body.expert
+    });
 
-  newArticle.save((err, article) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json({
-        message: "Article has been added",
-        newArticle
-      });
-    }
-  });
+    newArticle.save((err, article) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json({
+                message: "Article has been added",
+                newArticle
+            });
+        }
+    });
 });
 
 //insert news
 router.post("/NewsPost", (req, res, next) => {
-  let newNews = new news({
-    source: req.body.source,
-    date: new Date(),
-    title: req.body.title,
-    detail: req.body.detail
-  });
+    let newNews = new news({
+        source: req.body.source,
+        date: new Date(),
+        title: req.body.title,
+        detail: req.body.detail
+    });
 
-  newNews.save((err, newNews) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json({
-        message: "News has been added",
-        newNews
-      });
-    }
-  });
+    newNews.save((err, newNews) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json({
+                message: "News has been added",
+                newNews
+            });
+        }
+    });
 });
 
 //insert knowledge question
 
 router.post("/questionPost", upload.array("image", 3), (req, res, next) => {
-  let imagePath = [];
-  if (req.files != null) {
-    for (let i = 0; i < req.files.length; i++) {
-      imagePath.push(req.files[i].path.replace("\\", "/"));
+    let imagePath = [];
+    if (req.files != null) {
+        for (let i = 0; i < req.files.length; i++) {
+            imagePath.push(req.files[i].path.replace("\\", "/"));
+        }
     }
-  }
-  let newQuestion = new questions({
-    question: req.body.question,
-    responseType: req.body.responseType,
-    type: req.body.type,
-    count: req.body.count,
-    image: imagePath,
-    option: req.body.option
-  });
+    let newQuestion = new questions({
+        question: req.body.question,
+        responseType: req.body.responseType,
+        type: req.body.type,
+        count: req.body.count,
+        image: imagePath,
+        option: req.body.option
+    });
 
-  newQuestion.save((err, newQuestion) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json({
-        message: "Question has been added",
-        newQuestion
-      });
-    }
-  });
+    newQuestion.save((err, newQuestion) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json({
+                message: "Question has been added",
+                newQuestion
+            });
+        }
+    });
 });
 
 router.post("/response", (req, res, next) => {
-  res.json({
-    message: "Response recieved",
-    response: req.body.response
-  });
+    res.json({
+        message: "Response recieved",
+        response: req.body.response
+    });
 });
 
 //updating data
 router.put("/put_route", (req, res, next) => {
-  //to do later
+    //to do later
 });
 
 //deleting data
 router.delete("/delete_route", (req, res, next) => {
-  //to do later
+    //to do later
 });
 module.exports = router;
