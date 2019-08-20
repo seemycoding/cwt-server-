562421aea1eaef0925b9aca93cf7dbb056d43113
unknown <navneet>
2019-08-20 13:16:52 +0530

    error

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const logger = require("morgan");
 const cors = require("cors");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-const bodyParser = require('body-parser');
+
 
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
