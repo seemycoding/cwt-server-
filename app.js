7c91eb69a44bc852e55061468aa76f45ace3c1ab
unknown <navneet>
2019-08-20 13:23:05 +0530

    confilct

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(express.urlencoded({limit: '50mb', parameterLimit:50000,extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
