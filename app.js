fd37a4907c7695ae950a1f6aeb5fe032e6e1b5f7
unknown <navneet>
2019-08-20 12:58:31 +0530

    file-too-large

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
