dc2cd7bffdfeed8df084224bc46d59dbf31fdbbb
kshitij <ktiwari.0202@gmail.com>
2019-08-27 11:30:33 +0530

    commit

--- controllers/Highlights.js
+++ controllers/Highlights.js
@@ -28,7 +28,7 @@ const HighlightController = {
     var highlight;
     if(image) {
       highlight = new Highlight({
-        _id: req.body.id,
+        id: req.body.id,
         title: req.body.title,
         link: req.body.link,
         image: image,
@@ -37,7 +37,7 @@ const HighlightController = {
       });
     } else {
       highlight = new Highlight({
-        _id: req.body.id,
+        id: req.body.id,
         title: req.body.title,
         link: req.body.link,
         sortOrder: req.body.sortOrder,
