1688537b3ddacc3ee6b0b7318861ff1de8e0a9b5
Himanshu kumar <hvk940@gmail.com>
2020-04-17 15:45:34 +0530

    db url change

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
-const dbUrl = process.env.DB_URL || 'mongodb://cwtUser:cleanwatertogether@localhost:27017/cwt?authSource=cwt'
+//const dbUrl = process.env.DB_URL || 'mongodb://cwtUser:cleanwatertogether@localhost:27017/cwt?authSource=cwt'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/cwt';
 const dbConnetion = mongoose.createConnection(dbUrl);
 module.exports = dbConnetion;
