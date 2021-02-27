7c4a9629c4c9b2ff450a4c84b5b115ab036b3e74
Himanshu kumar <hvk940@gmail.com>
2021-02-27 16:12:03 +0530

    push

--- config/database.js
+++ config/database.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose')
 //const dbUrl = process.env.DB_URL || 'mongodb://cwtUser:cleanwatertogether@localhost:27017/cwt?authSource=cwt'
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/cwt';
-var options =  {
-    useMongoClient: true,
-    keepAlive: 1,
-    connectTimeoutMS: 30000,
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 5000,
-    useNewUrlParser: true
- }
-const dbConnetion = mongoose.createConnection(dbUrl,options );
-module.exports = dbConnetion;
+const client = mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//db connected
+client.then((res) => {
+  console.log(res);
+  console.log("Connected to mongodb");
+});
